ea1345c2b92e8624362aa1348d2bc9c0850203e3
EBETH JAMIN MEJIA CHAVEZ <l22590434@sjuanrio.tecnm.mx>
2026-03-07 22:17:32 -0600

    Refactor LoginRequest class for explicit methods
    Refactor LoginRequest class to remove Lombok annotations and add explicit getters and setters.

--- src/main/java/com/cherrytwins/shop/security/dto/LoginRequest.java
+++ src/main/java/com/cherrytwins/shop/security/dto/LoginRequest.java
@@ -2,17 +2,21 @@ package com.cherrytwins.shop.security.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class LoginRequest {
 
-    @Email @NotBlank
-    private  String email;
+    @Email
+    @NotBlank
+    private String email;
 
     @NotBlank
     private String password;
 
+    public LoginRequest() {}
+
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
 }
