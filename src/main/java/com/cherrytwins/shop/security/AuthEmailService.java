0fca6772a560aa286003e0fc96be0f3e5035f16e
EBETH JAMIN MEJIA CHAVEZ <l22590434@sjuanrio.tecnm.mx>
2026-05-24 20:40:32 -0600

    Import Propagation for transaction management

--- src/main/java/com/cherrytwins/shop/security/AuthEmailService.java
+++ src/main/java/com/cherrytwins/shop/security/AuthEmailService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.OffsetDateTime;
+import org.springframework.transaction.annotation.Propagation;
 import java.util.Map;
 
 @Service
